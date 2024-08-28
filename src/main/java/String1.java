8ef6ec865acc045ad39a0b64d7fdfb4cba4f5338
Abeysinghe_A <151038579+avinAbeysinghe@users.noreply.github.com>
2024-08-27 22:18:33 -0700

    Updated extraEnd

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -87,7 +87,8 @@ public class String1
      * extraEnd("Hi") → "HiHiHi"
      */
     public String extraEnd(String str) {
-        return str.substring(str.length()-2,str.length()) + str.substring(str.length()-2,str.length()) + str.substring(str.length()-2,str.length());
+        String process = str.substring(str.length()-2,str.length());
+        return process + process + process;
     }
 
     /*
