df0c3dd2da9882cc9c526b2ea3413bba00521edb
Abeysinghe_A <151038579+avinAbeysinghe@users.noreply.github.com>
2024-08-27 21:51:56 -0700

    Update String1.java
    fixed concat : still debugging extraEnd

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -201,7 +201,8 @@ public class String1
      * conCat("abc", "") → "abc"
      */
     public String conCat(String a, String b) {
-        if(a.substring(a.length()-1).equals(b.substring(0, 1))){
+        if(b.length() == 0){return a;}
+        else if(a.substring(a.length()-1).equals(b.substring(0, 1))){
             return a.substring(0, a.length()-1) + b.substring(0, b.length());
         } else {return a + b;}
     }
